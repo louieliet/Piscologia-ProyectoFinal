e95d514c34ac4677ccbe304fa3168f573e45b24f
louieliet <louieliet2003@gmail.com>
2023-05-07 11:47:41 -0600

    updated

--- Scripts/MallaFacial.py
+++ Scripts/MallaFacial.py
@@ -63,51 +63,78 @@ while True:
                 xo, yo = int(points.x * width), int(points.y * height)
                 lista.append([id, xo, yo])
 
-        
                 #cv2.putText(frame, str(id), (xo,yo), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0), 1)
 
                 if len(lista) == 468 and distance < 278.76:
-
-                    #Felicidad
-                    
+    
                     #Mejilla izq - Ojo izq
                     x, y = lista[266][1:]
                     x1, y1 = lista[374][1:]
                     d1 = distanceCalculator(x, x1, y, y1)
-                    #cv2.line(frame, (x,y), (x1,y1), (0,0,0), 3)
 
                     #Boca apertura 
                     x2, y2 = lista[185][1:]
                     x3, y3 = lista[409][1:]
                     d2 = distanceCalculator(x2, x3, y2, y3)
-                    #cv2.line(frame, (x2,y2), (x3,y3), (0,0,0), 3)
-                    
+
 
                     #Mejilla der - Ojo der
                     x4, y4 = lista[36][1:]
                     x5, y5 = lista[145][1:]
                     d3 = distanceCalculator(x4, x5, y4, y5)
-                    #cv2.line(frame, (x4,y4), (x5,y5), (0,0,0), 3)
 
 
-                    #Ojera izq - Ojo izq
-                    x6, y6 = lista[449][1:]
-                    x7, y7 = lista[373][1:]
+                    #Gallo der
+                    x6, y6 = lista[265][1:]
+                    x7, y7 = lista[340][1:]
                     d4 = distanceCalculator(x6, x7, y6, y7)
-                    #cv2.line(frame, (x6,y6), (x7,y7), (0,0,0), 3)
 
-                    #Ojera der - Ojo der
-                    x8, y8 = lista[229][1:]
-                    x9, y9 = lista[144][1:]
+
+
+                    #Gallo izq
+                    x8, y8 = lista[35][1:]
+                    x9, y9 = lista[111][1:]
                     d5 = distanceCalculator(x8, x9, y8, y9)
 
-                    promedio = (d1 + d2 + d3 + d4 + d5) / 5
+                    #Parte superior del ojo izq, parte inf del ojo izq
+                    x10, y10 = lista[386][1:]
+                    x11, y11 = lista[374][1:]
+                    d6 = distanceCalculator(x10, x11, y10, y11)
 
+                    #Parte superior del ojo der, parte inf del ojo der
+                    x12, y12 = lista[160][1:]
+                    x13, y13 = lista[223][1:]
+                    d7 = distanceCalculator(x12, x13, y12, y13)
+
+                    #Apertura mejillas der
+                    x14, y14 = lista[287][1:]
+                    x15, y15 = lista[427][1:]
+                    d8 = distanceCalculator(x14, x15, y14, y15)
+
+                    #Apertura mejillas izq
+                    x16, y16 = lista[186][1:]
+                    x17, y17 = lista[205][1:]
+                    d9 = distanceCalculator(x14, x15, y14, y15)
+                    
+
+
+
+                    cv2.line(frame, (x,y), (x1,y1), (0,0,0), 3)
+                    cv2.line(frame, (x2,y2), (x3,y3), (0,0,0), 3)
+                    cv2.line(frame, (x4,y4), (x5,y5), (0,0,0), 3)
+                    cv2.line(frame, (x6,y6), (x7,y7), (0,0,0), 3)
+                    cv2.line(frame, (x8,y8), (x9,y9), (0,0,0), 3)
+                    cv2.line(frame, (x10,y10), (x11,y11), (0,0,0), 3)
+                    cv2.line(frame, (x12,y12), (x13,y13), (0,0,0), 3)
+                    cv2.line(frame, (x14,y14), (x15,y15), (0,0,0), 3)
+                    cv2.line(frame, (x16,y16), (x17,y17), (0,0,0), 3)
+
+
+
+                    promedio = (d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8) / 8
                     print(promedio)
 
-                    if (promedio < 22 and promedio > 20):
-                        cv2.putText(frame, "Feliz", (480, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 1)
-                
+                    
                 if distance > 218.76:
                     cv2.putText(frame, "No se puede detectar", (480, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 1)
                     
