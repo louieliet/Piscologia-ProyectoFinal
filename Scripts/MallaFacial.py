069168708f3abacf0d984feaf03b3b25f2d88180
louieliet <louieliet2003@gmail.com>
2023-05-08 20:28:26 -0600

    updated

--- Scripts/MallaFacial.py
+++ Scripts/MallaFacial.py
@@ -63,79 +63,94 @@ while True:
                 xo, yo = int(points.x * width), int(points.y * height)
                 lista.append([id, xo, yo])
 
-                #cv2.putText(frame, str(id), (xo,yo), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0), 1)
+                cv2.putText(frame, str(id), (xo,yo), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,0,0), 1)
 
-                if len(lista) == 468 and distance < 278.76:
-    
-                    #Mejilla izq - Ojo izq
-                    x, y = lista[266][1:]
-                    x1, y1 = lista[374][1:]
-                    d1 = distanceCalculator(x, x1, y, y1)
-
-                    #Boca apertura 
-                    x2, y2 = lista[185][1:]
-                    x3, y3 = lista[409][1:]
-                    d2 = distanceCalculator(x2, x3, y2, y3)
-
-
-                    #Mejilla der - Ojo der
-                    x4, y4 = lista[36][1:]
-                    x5, y5 = lista[145][1:]
-                    d3 = distanceCalculator(x4, x5, y4, y5)
-
-
-                    #Gallo der
-                    x6, y6 = lista[265][1:]
-                    x7, y7 = lista[340][1:]
-                    d4 = distanceCalculator(x6, x7, y6, y7)
-
-
-
-                    #Gallo izq
-                    x8, y8 = lista[35][1:]
-                    x9, y9 = lista[111][1:]
-                    d5 = distanceCalculator(x8, x9, y8, y9)
-
-                    #Parte superior del ojo izq, parte inf del ojo izq
-                    x10, y10 = lista[386][1:]
-                    x11, y11 = lista[374][1:]
-                    d6 = distanceCalculator(x10, x11, y10, y11)
-
-                    #Parte superior del ojo der, parte inf del ojo der
-                    x12, y12 = lista[160][1:]
-                    x13, y13 = lista[223][1:]
-                    d7 = distanceCalculator(x12, x13, y12, y13)
-
-                    #Apertura mejillas der
-                    x14, y14 = lista[287][1:]
-                    x15, y15 = lista[427][1:]
-                    d8 = distanceCalculator(x14, x15, y14, y15)
-
-                    #Apertura mejillas izq
-                    x16, y16 = lista[186][1:]
-                    x17, y17 = lista[205][1:]
-                    d9 = distanceCalculator(x14, x15, y14, y15)
+                if len(lista) == 468 and distance < 82:
                     
+                    #Felicidad
+
+                    #Boca extremos
+                    x1, y1 = lista[308][1:]
+                    x2, y2 = lista[61][1:]
+                    cv2.line(frame, (x1, y1), (x2, y2), (0,0,255), 3)
+                    distanciaBocaExtremo = distanceCalculator(x1,x2,y1,y2)
+
+                    #Boca apertura
+                    x3, y3 = lista[13][1:]
+                    x4, y4 = lista[14][1:]
+                    cv2.line(frame, (x3, y3), (x4, y4), (0,0,255), 3)
+                    distanciaBocaApertura = distanceCalculator(x3,x4,y3,y4)
+
+                    #Extremo ojo der - Ceja Derecha
+                    x5, y5 = lista[65][1:]
+                    x6, y6 = lista[158][1:]
+                    cv2.line(frame, (x5, y5), (x6, y6), (0,0,255), 3)
+                    distanciaCejaDer = distanceCalculator(x5,x6,y5,y6)
+
+                    #Extremo ojo izq - Ceja Izq
+                    x7, y7 = lista[295][1:]
+                    x8, y8 = lista[385][1:]
+                    cv2.line(frame, (x7, y7), (x8, y8), (0,0,255), 3)
+                    distanciaCejaIzq = distanceCalculator(x7,x8,y7,y8)
+
+                    distanciaCejas = (distanciaCejaDer + distanciaCejaIzq) / 2
+
+                    #Mejilla Izquierda
+                    x9, y9 = lista[410][1:]
+                    x10, y10 = lista[427][1:]
+                    cv2.line(frame, (x9, y9), (x10, y10), (0,0,255), 3)
+                    distainciaMejillaIzq = distanceCalculator(x9,x10,y9,y10)
+
+                    #Mejilla Der
+                    x11, y11 = lista[186][1:]
+                    x12, y12 = lista[207][1:]
+                    cv2.line(frame, (x11, y11), (x12, y12), (0,0,255), 3)
+                    distainciaMejillaDer  = distanceCalculator(x11,x12,y11,y12)
+
+                    distanciaMejillas = (distainciaMejillaDer + distainciaMejillaIzq) / 2
+
+                    #Pata Gallo Izq 
+                    x13, y13 = lista[346][1:]
+                    x14, y14 = lista[340][1:]
+                    cv2.line(frame, (x13, y13), (x14, y14), (0,0,255), 3)
+                    distanciaPataGalloIzq  = distanceCalculator(x13,x14,y13,y14)
+
+                    #Pata Gallo Der 
+                    x15, y15 = lista[117][1:]
+                    x16, y16 = lista[111][1:]
+                    cv2.line(frame, (x15, y15), (x16, y16), (0,0,255), 3)
+                    distanciaPataGalloDer  = distanceCalculator(x15,x16,y15,y16)
+
+                    distanciaPataGallo = (distanciaPataGalloDer + distanciaPataGalloIzq) / 2
+
+                    #Pata Gallo Izq 2 
+                    x17, y17 = lista[448][1:]
+                    x18, y18 = lista[342][1:]
+                    cv2.line(frame, (x17, y17), (x18, y18), (0,0,255), 3)
+                    distanciaPataGalloIzq2  = distanceCalculator(x17,x18,y17,y18)
+
+                    #Pata Gallo Der 2 
+                    x19, y19 = lista[228][1:]
+                    x20, y20 = lista[113][1:]
+                    cv2.line(frame, (x19, y19), (x20, y20), (0,0,255), 3)
+                    distanciaPataGalloDer2  = distanceCalculator(x19,x20,y19,y20)
+
+                    distanciaPataGallo2 = (distanciaPataGalloIzq2 + distanciaPataGalloDer2) / 2
+
+
+                    if( 
+                        distanciaBocaExtremo     > 120.0 and 
+                        distanciaBocaApertura    > 23.0  and
+                        distanciaCejas           > 30.0  and
+                        distanciaMejillas        < 34.0  and
+                        distanciaPataGallo       < 12.0  and
+                        distanciaPataGallo2      < 35.0):
+                        emotion = "Feliz"
+                        cv2.putText(frame, str(emotion), (500, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,0), 10)
 
 
 
-                    cv2.line(frame, (x,y), (x1,y1), (0,0,0), 3)
-                    cv2.line(frame, (x2,y2), (x3,y3), (0,0,0), 3)
-                    cv2.line(frame, (x4,y4), (x5,y5), (0,0,0), 3)
-                    cv2.line(frame, (x6,y6), (x7,y7), (0,0,0), 3)
-                    cv2.line(frame, (x8,y8), (x9,y9), (0,0,0), 3)
-                    cv2.line(frame, (x10,y10), (x11,y11), (0,0,0), 3)
-                    cv2.line(frame, (x12,y12), (x13,y13), (0,0,0), 3)
-                    cv2.line(frame, (x14,y14), (x15,y15), (0,0,0), 3)
-                    cv2.line(frame, (x16,y16), (x17,y17), (0,0,0), 3)
-
-
-
-                    promedio = (d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8) / 8
-                    print(promedio)
-
-                    
-                if distance > 218.76:
+                if distance > 82:
                     cv2.putText(frame, "No se puede detectar", (480, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 1)
                     
                     
