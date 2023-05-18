228b9c2494ca90498ae6439ed9f0e8f888e62c85
louieliet <louieliet2003@gmail.com>
2023-05-17 20:05:41 -0600

    updated

--- App/test.py
+++ App/test.py
@@ -1,21 +1,13 @@
-#--------------Inicio------------#
-#import kivy
-from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import builder
+
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
-class Ui(ScreenManager):
+class liz(BoxLayout):
     pass
-class MainApp(MDApp):
+
+class TutorialApp(App):
     def build(self):
-        self.theme_cls.theme_style='Dark'
-        self.theme_cls.primary_palette ='Teal'
-        builder.load_file('desing.kv')
-        return Ui()
+        return liz()
 
-
-
-if __name__=="__main__":
-    MainApp().run()
-
+if __name__ == "__main__":
+    TutorialApp().run()
--- KivyTestEmi/test1.py
+++ KivyTestEmi/test1.py
@@ -1,18 +1,13 @@
 
 from kivy.app import App
-
-from kivy.uix.scatter import Scatter
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 
-class ScatterTextWidget(BoxLayout):
+class a123(BoxLayout):
     pass
 
 class TutorialApp(App):
     def build(self):
-        return ScatterTextWidget()
+        return a123()
 
 if __name__ == "__main__":
     TutorialApp().run()
