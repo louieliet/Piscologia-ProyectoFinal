3813ab79171cc179e703e093ba7a62397f349bff
Lizbethrts <lizzytrujillosalgado130503@gmail.com>
2023-05-17 20:54:26 -0600

    avance

--- App/test.py
+++ App/test.py
@@ -1,13 +1,30 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+#---------Liz
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivy.uix.button import Button
 
-class liz(BoxLayout):
+class Ui(ScreenManager):
     pass
 
-class TutorialApp(App):
+class MainApp(MDApp):
     def build(self):
-        return liz()
+        #self.theme_cls.theme_style='Dark'
+        #self.theme_cls.primary_palette='Teal'
+        button=Button (background_normal='botones.png')
+        Builder.load_file('tutorial.kv')
+        return Ui()
+    def change_style(self,checked,value):
+        if value:
+            self.theme_cls.theme_style='Dark'
+        else:
+            self.theme_cls.theme_style='Ligth'
+
+
+
 
 if __name__ == "__main__":
-    TutorialApp().run()
+    MainApp().run()
